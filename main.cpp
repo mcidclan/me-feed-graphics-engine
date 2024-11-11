ad2b835252a350f7b8bca6ab2dc36e489a06fc9e
m-c/d <m.cid.clan@gmail.com>
2024-11-11 12:23:16 +0100

    - refactor code for clarity, - fix memory overflow, - clean up code, - draw a square from main cpu list.

--- main.cpp
+++ main.cpp
@@ -4,100 +4,51 @@ PSP_MODULE_INFO("megu", 0, 1, 1);
 PSP_HEAP_SIZE_KB(-1024);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_VFPU | PSP_THREAD_ATTR_USER);
 
-#define MAX_LIST_MEMORY 2048
-
-volatile u32* meListBase = nullptr;
-volatile u32* meList = nullptr;
-void meListSetMemory(const bool clean = false) {
-  static u32* _base = nullptr;
-  if (!_base) {
-    _base = (u32*)memalign(16, MAX_LIST_MEMORY);
-    memset(_base, 0, MAX_LIST_MEMORY); 
-  }
-  if (!clean) {
-    meListBase = (u32*)(0x40000000 | (u32)_base);
-    meList = meListBase;
-    return;
-  }
-  free(_base);
-}
-
-inline void* getListMemory(const u32 size, const bool reset) {
- static volatile u32 cursor = MAX_LIST_MEMORY;
- if (reset) {
-  cursor = MAX_LIST_MEMORY;
- }
- cursor -= size;
- return (void*)(cursor + (u32)meListBase);
-}
-
-static u32 cmdCursor = 0;
-inline void sendGeCommand(const u32 cmd, const u32 value) {
-  meList[cmdCursor++] = (cmd << 24) | (value & 0x00FFFFFF);
-}
-
-static bool refreshMeList = false;
-u32* meListSwitch() {
-  u32* const _meList = (u32* const)meList;
-  if (refreshMeList) {
-    static u32 offset = 0;
-    offset ^= (((u32)MAX_LIST_MEMORY) >> 1);
-    meList = (u32*)(((u32)meList) + offset);
-    refreshMeList = false;
-  }
-  return _meList;
-}
-
-static void meListWaitRefresh() {
-  while(!refreshMeList) {
-    meDCacheWritebackInvalidAll();
-    sceKernelDelayThread(1);
-  }
-}
-
-void writeMeList() {
+static void writeMeList() {
   static u8 r = 0x7f;
   static u8 g = 0x7f;
   static u8 b = 0x7f;
-  if (!refreshMeList) {
-    Vertex* vertices = (Vertex*)getListMemory(sizeof(Vertex) * 2, true);
+  static u16 _r = 0;
+  static u16 _g = 0;
+  static u16 _b = 0;
+  static u16 _d = 0;
+  if (!meListRefresh) {
+    Vertex* vertices = (Vertex*)meListGetMemory(sizeof(Vertex) * 2, true);
     
-    randInRange(0xff) > 0x7f ? b < 0xfe ? b++ : b-- : b > 1 ? b-- : b++;
-    randInRange(0xff) > 0x7f ? g < 0xfe ? g++ : g-- : g > 1 ? g-- : g++;
-    randInRange(0xff) > 0x7f ? r < 0xfe ? r++ : r-- : r > 1 ? r-- : r++;
+    if(_d++ > 100) {
+      _r = randInRange(4);
+      _g = randInRange(4);
+      _b = randInRange(4);
+      _d = 0;
+    }
+    _r >= 2 ? b < 0xfe ? b++ : b-- : b > 1 ? b-- : b++;
+    _g >= 2 ? g < 0xfe ? g++ : g-- : g > 1 ? g-- : g++;
+    _b >= 2 ? r < 0xfe ? r++ : r-- : r > 1 ? r-- : r++;
     meDCacheWritebackInvalidAll();
+
     asm("sync");
-    
     vertices[1].color = 0xFF000000 | b << 16 | g << 8 | r;
     vertices[1].x = 480;
     vertices[1].y = 272;
-    
-    cmdCursor = 0;
     asm("sync");
     
-    sendGeCommand(0xd4, (16 << 10) | 0);                                          // CMD_SCISSOR1
-    sendGeCommand(0xd5, ((SCR_HEIGHT - 16) << 10) | SCR_WIDTH);                   // CMD_SCISSOR2
-    sendGeCommand(0xd3, (0b101 << 8) | 0x01);                                     // CMD_CLEAR
-    sendGeCommand(0x12, GU_COLOR_8888 | GU_VERTEX_16BIT | GU_TRANSFORM_2D);
-    sendGeCommand(0x10, (((u32)vertices) >> 8) & 0xf0000);
-    sendGeCommand(0x01, ((u32)vertices) & 0xffffff);
-    sendGeCommand(0x04, (GU_SPRITES << 16) | 2);
-    sendGeCommand(0xd3, 0);                                                       // CMD_CLEAR
-    sendGeCommand(0x0b, 0);                                                       // CMD_RET
-    refreshMeList = true;
+    cmdCursor = 0;
+    sendGeCommand(0xd4, (16 << 10) | 0);                                        // CMD_SCISSOR1
+    sendGeCommand(0xd5, ((SCR_HEIGHT - 16) << 10) | SCR_WIDTH);                 // CMD_SCISSOR2
+    sendGeCommand(0xd3, (0b101 << 8) | 0x01);                                   // CMD_CLEAR, clear before drawing just in case
+    sendGeCommand(0x12, GU_COLOR_8888 | GU_VERTEX_16BIT | GU_TRANSFORM_2D);     // CMD_VTYPE
+    sendGeCommand(0x01, ((u32)vertices) & 0xffffff);                            // CMD_VADR, vertex data addr
+    sendGeCommand(0x04, (GU_SPRITES << 16) | 2);                                // CMD_PRIM, draw
+    sendGeCommand(0xd3, 0);                                                     // CMD_CLEAR
+    sendGeCommand(0x0b, 0);                                                     // CMD_RET
+    meListRefresh = true;
     asm("sync");
   }
 }
 
 // me
-volatile bool _meExit = false;
-static void meExit() {
-  _meExit = true;
-  meDCacheWritebackInvalidAll();
-}
-
 volatile u32 counter = 0;
-int meLoop() {
+static int meLoop() {
   do {
     writeMeList();
     counter++;
@@ -110,14 +61,9 @@ extern char __start__me_section;
 extern char __stop__me_section;
 __attribute__((section("_me_section")))
 void meHandler() {
-  vrg(0xbc100050) |= 0x7;       // transfer bus control bits
+  vrg(0xbc100050) |= 0x07;      // enable clocks: ME + AW bus RegA & RegB (bits 0-2)
   vrg(0xbc100004) = 0xFFFFFFFF; // clear all interrupts
   vrg(0xbc100040) = 0x01;       // allow 32MB ram
-  asm("sync");
-  vrp ptr = (vrp)0xBC000000;    // disable memory protections
-  while (ptr <= (vrp)0xBC00000C) {
-    *(ptr++) = 0xFFFFFFFF;
-  }
   asm("sync");
   ((FCall)_meLoop)();
 }
@@ -126,7 +72,7 @@ static int meInit() {
   memcpy((void *)0xbfc00040, (void*)&__start__me_section, me_section_size);
   _meLoop = (u32)&meLoop;
   meDCacheWritebackInvalidAll();
-  vrg(0xBC10004C) |= 0x04;        // 0b0100;  // reset enable, just the me
+  vrg(0xBC10004C) |= 0x04;        // reset enable, just the me
   asm("sync");
   vrg(0xBC10004C) = 0x0;          // disable reset to start the me
   asm("sync"); 
@@ -134,11 +80,11 @@ static int meInit() {
 }
 
 // gu
-u32* mclist = nullptr;
+static unsigned int __attribute__((aligned(16))) mcList[MAX_LIST_MEMORY] = {0};
 
 void guInit() {
   sceGuInit();
-  sceGuStart(GU_DIRECT, mclist);
+  sceGuStart(GU_DIRECT, mcList);
   sceGuDrawBuffer(GU_PSM_8888, (void*)0x0, BUF_WIDTH);
   sceGuDispBuffer(SCR_WIDTH, SCR_HEIGHT, (void*)0x88000, BUF_WIDTH);
   sceGuDepthBuffer((void*)0x110000, BUF_WIDTH);
@@ -149,25 +95,9 @@ void guInit() {
   sceGuSync(0,0);
 }
 
-inline void start() {
-  sceGuStart(GU_DIRECT, mclist);
-  sceGuClear(GU_COLOR_BUFFER_BIT|GU_DEPTH_BUFFER_BIT);
-}
-
-inline void end() {
-  sceGuFinish();
-  sceGuSync(0,0);
-  sceDisplayWaitVblankStart();
-  sceGuSwapBuffers();
-}
-
 // main
-int main(int argc, char **argv) {
+int main() {
   scePowerSetClockFrequency(333, 333, 166);
-
-  meListSetMemory();
-  mclist = (u32*)memalign(16, MAX_LIST_MEMORY);
-  
   if (pspSdkLoadStartModule("ms0:/PSP/GAME/me/kcall.prx", PSP_MEMORY_PARTITION_KERNEL) < 0){
     sceKernelExitGame();
     return 0;
@@ -176,21 +106,40 @@ int main(int argc, char **argv) {
   guInit();
   pspDebugScreenInitEx(0x0, PSP_DISPLAY_PIXEL_FORMAT_8888, 0);
   pspDebugScreenSetOffset(0);
+
+  meListSetMemory();
   kcall(&meInit);
   meListWaitRefresh();
   
   SceCtrlData ctl;
   do {
-    sceKernelDcacheWritebackInvalidateAll();
-    
-    start();
+    sceGuStart(GU_DIRECT, mcList);
+    sceGuClear(GU_COLOR_BUFFER_BIT|GU_DEPTH_BUFFER_BIT);
     pspDebugScreenSetXY(0, 1);
     pspDebugScreenPrintf("%u                                       ", counter);
     sceCtrlPeekBufferPositive(&ctl, 1);
     const u32* const list = meListSwitch();
     sceGuCallList(list);
-    end();
-    
+    {
+      static int dir = 1;
+      static int move = 0;
+      move += dir;
+      if(move > 32) {
+        dir = -1;
+      } else if(move < -32) {
+        dir = 1;
+      }
+      Vertex vertices[] = {
+        {0, (u16)(176+move), 72, 0, 0},
+        {0xFF0000FF, (u16)(128+176+move), 128+72, 0, 0},
+      };
+      sceGuDrawArray(GU_SPRITES, GU_COLOR_8888 | GU_VERTEX_16BIT | GU_TRANSFORM_2D, 2, nullptr, vertices);
+    }
+    sceGuFinish();
+    sceGuSync(0,0);
+    sceKernelDcacheWritebackInvalidateAll();
+    sceDisplayWaitVblankStart();
+    sceGuSwapBuffers();
   } while(!(ctl.Buttons & PSP_CTRL_HOME));
   
   meExit();
@@ -198,10 +147,8 @@ int main(int argc, char **argv) {
   pspDebugScreenSetXY(0, 1);
   pspDebugScreenPrintf("Exiting...");
   sceKernelDelayThread(500000);
-  meListSetMemory(false);
-  // free(mclist);
-  // free((void*)meList);
   
+  meListSetMemory(false);
   sceKernelExitGame();
   return 0;
 }
--- main.h
+++ main.h
@@ -22,12 +22,19 @@
 
 // me
 // __attribute__((optimize("unroll-loops")))
-inline void meDCacheWritebackInvalidAll() {
+static inline void meDCacheWritebackInvalidAll() {
+ asm("sync");
  for (int i = 0; i < 8192; i += 64) {
   asm("cache 0x14, 0(%0)" :: "r"(i));
   asm("cache 0x14, 0(%0)" :: "r"(i));
  }
  asm("sync");
+}
+
+static volatile bool _meExit = false;
+static inline void meExit() {
+  _meExit = true;
+  meDCacheWritebackInvalidAll();
 }
 
 // gu
@@ -40,3 +47,59 @@ struct Vertex {
   u16 x, y, z;
   u16 pad;
 } __attribute__((aligned(4)));
+
+// me list & ge
+#define MAX_LIST_MEMORY 2048
+static volatile u32* meListBase = nullptr;
+static volatile u32* meList = nullptr;
+
+static volatile u32 cmdCursor = 0;
+static inline void sendGeCommand(const u32 cmd, const u32 value) {
+  asm("sync");
+  meList[cmdCursor++] = (cmd << 24) | (value & 0x00FFFFFF);
+  asm("sync");
+}
+
+static inline void meListSetMemory(const bool clean = false) {
+  static u32* _base = nullptr;
+  if (!_base) {
+    _base = (u32*)memalign(16, MAX_LIST_MEMORY);
+    memset(_base, 0, MAX_LIST_MEMORY); 
+  }
+  if (!clean) {
+    meListBase = (u32*)(0x40000000 | (u32)_base);
+    meList = meListBase;
+    return;
+  }
+  free(_base);
+}
+
+static inline void* meListGetMemory(const u32 size, const bool reset) { //
+ static u32 cursor = MAX_LIST_MEMORY;
+ if (reset) {
+  cursor = MAX_LIST_MEMORY;
+ }
+ cursor -= size;
+ return (void*)(cursor + (u32)meListBase);
+}
+
+static volatile bool meListRefresh = false;
+static inline u32* meListSwitch() {
+  u32* const _meList = (u32*)meList;
+  if (meListRefresh) {
+    static u32 offset = 0;
+    offset ^= (((u32)MAX_LIST_MEMORY) >> 1);
+    asm("sync");
+    meList = (u32*)(((u32)meListBase) + offset);
+    meListRefresh = false;
+    asm("sync");
+  }
+  return _meList;
+}
+
+static inline void meListWaitRefresh() {
+  while(!meListRefresh) {
+    meDCacheWritebackInvalidAll();
+    sceKernelDelayThread(1);
+  }
+}
