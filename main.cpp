59ebbc9ea3974c4a83ef50ed2b313fe1703eff7f
m-c/d <534348+mcidclan@users.noreply.github.com>
2025-06-22 17:04:26 +0200

    fix allocation writeback and apply optimization on host memory usage, remove redundant Me cpu cache operations

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ static void writeMeList() {
   if (!meListRefresh) {
     Vertex* vertices = (Vertex*)meListGetMemory(sizeof(Vertex) * 2, true);
     
-    if(_d++ > 100) {
+    if (_d++ > 100) {
       _r = randInRange(4);
       _g = randInRange(4);
       _b = randInRange(4);
@@ -25,8 +25,7 @@ static void writeMeList() {
     _r >= 2 ? b < 0xfe ? b++ : b-- : b > 1 ? b-- : b++;
     _g >= 2 ? g < 0xfe ? g++ : g-- : g > 1 ? g-- : g++;
     _b >= 2 ? r < 0xfe ? r++ : r-- : r > 1 ? r-- : r++;
-    meDCacheWritebackInvalidAll();
-
+    
     vertices[1].color = 0xFF000000 | b << 16 | g << 8 | r;
     vertices[1].x = 480;
     vertices[1].y = 272;
@@ -40,40 +39,55 @@ static void writeMeList() {
     sendGeCommand(0x04, (GU_SPRITES << 16) | 2);                                // CMD_PRIM, draw
     sendGeCommand(0xd3, 0);                                                     // CMD_CLEAR
     sendGeCommand(0x0b, 0);                                                     // CMD_RET
-    meListRefresh = true;
+    meListRefresh = 1;
   }
 }
 
 // me
-volatile u32 counter = 0;
-static int meLoop() {
+static void meLoop() {
+  do {
+    meDCacheWritebackInvalidAll();
+  } while(!mem);
+  
   do {
     writeMeList();
-    counter++;
-    meDCacheWritebackInvalidAll();
-  } while(!_meExit);
-  return _meExit;
+    meCounter++;
+  } while(meExit == 0);
+  meExit = 2;
+  meHalt();
 }
 
 extern char __start__me_section;
 extern char __stop__me_section;
 __attribute__((section("_me_section")))
 void meHandler() {
-  vrg(0xbc100050) |= 0x07;      // enable clocks: ME + AW bus RegA & RegB (bits 0-2)
-  vrg(0xbc100004) = 0xFFFFFFFF; // clear all interrupts
-  vrg(0xbc100040) = 0x01;       // allow 32MB ram
+  hw(0xbc100050) = 0x0f;        // enable clocks: ME + AW bus RegA & RegB (bits 0-2)
+  hw(0xbc100004) = 0xffffffff;  // Enable NMIs
+  hw(0xbc100040) = 0x02;        // allow 64MB ram
   asm("sync");
-  ((FCall)_meLoop)();
+  
+  asm volatile(
+    "li          $k0, 0x30000000\n"
+    "mtc0        $k0, $12\n"
+    "sync\n"
+    "la          $k0, %0\n"
+    "li          $k1, 0x80000000\n"
+    "or          $k0, $k0, $k1\n"
+    "jr          $k0\n"
+    "nop\n"
+    :
+    : "i" (meLoop)
+    : "k0"
+  );
 }
 
 static int meInit() {
-  memcpy((void *)0xbfc00040, (void*)&__start__me_section, me_section_size);
-  _meLoop = (u32)&meLoop;
-  meDCacheWritebackInvalidAll();
-  vrg(0xBC10004C) |= 0x04;        // reset enable, just the me
-  asm("sync");
-  vrg(0xBC10004C) = 0x0;          // disable reset to start the me
-  asm("sync"); 
+  #define me_section_size (&__stop__me_section - &__start__me_section)
+  memcpy((void *)ME_HANDLER_BASE, (void*)&__start__me_section, me_section_size);
+  sceKernelDcacheWritebackInvalidateAll();
+  hw(0xbc10004c) = 0x04;        // reset enable, just the me
+  hw(0xbc10004c) = 0x0;         // disable reset to start the me
+  asm volatile("sync");
   return 0;
 }
 
@@ -105,6 +119,9 @@ int main() {
   pspDebugScreenInitEx(0x0, PSP_DISPLAY_PIXEL_FORMAT_8888, 0);
   pspDebugScreenSetOffset(0);
 
+  meGetUncached32(&mem, 6);
+  sceKernelDcacheWritebackInvalidateAll();
+
   meListSetMemory();
   kcall(&meInit);
   meListWaitRefresh();
@@ -116,7 +133,7 @@ int main() {
     sceGuStart(GU_DIRECT, mcList);
     sceGuClear(GU_COLOR_BUFFER_BIT|GU_DEPTH_BUFFER_BIT);
     pspDebugScreenSetXY(0, 1);
-    pspDebugScreenPrintf("%u                                       ", counter);
+    pspDebugScreenPrintf("%u                                       ", meCounter);
     sceCtrlPeekBufferPositive(&ctl, 1);
     const u32* const list = meListSwitch();
     sceGuCallList(list);
@@ -145,13 +162,18 @@ int main() {
     sceKernelDcacheWritebackInvalidateAll();
   } while(!(ctl.Buttons & PSP_CTRL_HOME));
   
-  meExit();
+  meExit = 1;
+  do {
+    asm volatile("sync");
+  } while(meExit < 2);
+  
   pspDebugScreenClear();
   pspDebugScreenSetXY(0, 1);
   pspDebugScreenPrintf("Exiting...");
   sceKernelDelayThread(500000);
   
   meListSetMemory(false);
+  meGetUncached32(&mem, 0);
   sceKernelExitGame();
   return 0;
 }
--- main.h
+++ main.h
@@ -18,13 +18,16 @@
 #define nrp          u32*
 #define nrg(addr)    (*((nrp)(addr)))
 
-#define vrp          volatile u32*
-#define vrg(addr)    (*((vrp)(addr)))
-
-#define me_section_size (&__stop__me_section - &__start__me_section)
-#define _meLoop      vrg((0xbfc00040 + me_section_size))
+#define hwp          volatile u32*
+#define hw(addr)     (*((hwp)(addr)))
 
 // me
+#define UNCACHED_USER_MASK    0x40000000
+#define CACHED_KERNEL_MASK    0x80000000
+#define UNCACHED_KERNEL_MASK  0xA0000000
+
+#define ME_HANDLER_BASE       0xbfc00000
+
 // __attribute__((optimize("unroll-loops")))
 static inline void meDCacheWritebackInvalidAll() {
  asm("sync");
@@ -35,10 +38,24 @@ static inline void meDCacheWritebackInvalidAll() {
  asm("sync");
 }
 
-static volatile bool _meExit = false;
-static inline void meExit() {
-  _meExit = true;
-  meDCacheWritebackInvalidAll();
+inline void meHalt() {
+  asm volatile(".word 0x70000000");
+}
+
+inline void meGetUncached32(volatile u32** const mem, const u32 size) {
+  static void* _base = nullptr;
+  if (!_base) {
+    _base = memalign(16, size*4);
+    memset(_base, 0, size);
+    *mem = (u32*)(UNCACHED_USER_MASK | (u32)_base);
+    sceKernelDcacheWritebackInvalidateAll();
+    return;
+  } else if (!size) {
+    free(_base);
+  }
+  *mem = nullptr;
+  sceKernelDcacheWritebackInvalidateAll();
+  return;
 }
 
 // gu
@@ -53,13 +70,17 @@ struct Vertex {
 
 // me list & ge
 #define MAX_LIST_MEMORY 2048
-static volatile u32* meListBase = nullptr;
-static volatile u32* meList = nullptr;
+static volatile u32* mem = nullptr;
+#define meCounter         (mem[0])
+#define meExit            (mem[1])
+#define meListBase        (mem[2])
+#define meList            (mem[3])
+#define cmdCursor         (mem[4])
+#define meListRefresh     (mem[5])
 
-static volatile u32 cmdCursor = 0;
 static inline void sendGeCommand(const u32 cmd, const u32 value) {
   asm("sync");
-  meList[cmdCursor++] = (cmd << 24) | (value & 0x00FFFFFF);
+  ((u32*)meList)[cmdCursor++] = (cmd << 24) | (value & 0x00FFFFFF);
   asm("sync");
 }
 
@@ -67,10 +88,11 @@ static inline void meListSetMemory(const bool clean = false) {
   static u32* _base = nullptr;
   if (!_base) {
     _base = (u32*)memalign(16, MAX_LIST_MEMORY);
-    memset(_base, 0, MAX_LIST_MEMORY); 
+    memset(_base, 0, MAX_LIST_MEMORY);
+    sceKernelDcacheWritebackInvalidateAll();
   }
   if (!clean) {
-    meListBase = (u32*)(0x40000000 | (u32)_base);
+    meListBase = (UNCACHED_USER_MASK | (u32)_base);
     meList = meListBase;
     return;
   }
@@ -78,17 +100,16 @@ static inline void meListSetMemory(const bool clean = false) {
 }
 
 static inline void* meListGetMemory(const u32 size, const bool reset) { //
- static u32 cursor = MAX_LIST_MEMORY;
- if (reset) {
-  cursor = MAX_LIST_MEMORY;
- }
- cursor -= size;
- return (void*)(cursor + (u32)meListBase);
+  static u32 cursor = MAX_LIST_MEMORY;
+  if (reset) {
+    cursor = MAX_LIST_MEMORY;
+  }
+  cursor -= size;
+  return (void*)(cursor + meListBase);
 }
 
-static volatile bool meListRefresh = false;
 static inline u32* meListSwitch() {
-  u32* const _meList = (u32*)meList;
+  const u32 _meList = meList;
   // a lock/try-lock could have been used here, see me-sample-lock.
   // e.g., tryLock; const bool refresh = meListRefresh;
   // if (!refresh) { ... meListRefresh = true; }
@@ -97,16 +118,16 @@ static inline u32* meListSwitch() {
     static u32 offset = 0;
     offset ^= (((u32)MAX_LIST_MEMORY) >> 1);
     asm("sync");
-    meList = (u32*)(((u32)meListBase) + offset);
-    meListRefresh = false;
+    meList = meListBase + offset;
+    meListRefresh = 0;
     asm("sync");
   }
-  return _meList;
+  return (u32*)_meList;
 }
 
 static inline void meListWaitRefresh() {
-  while(!meListRefresh) {
-    meDCacheWritebackInvalidAll();
+  while (!meListRefresh) {
+    sceKernelDcacheWritebackInvalidateAll();
     sceKernelDelayThread(1);
   }
 }
